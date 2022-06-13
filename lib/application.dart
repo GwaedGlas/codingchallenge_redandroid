c85264ea50d058e58546640f22a0a16b39b69649
GwaedGlas <46369745+GwaedGlas@users.noreply.github.com>
2022-06-13 18:11:43 +0100

    Provided House repository

--- lib/application.dart
+++ lib/application.dart
@@ -1,5 +1,7 @@
+import 'package:codingchallenge_redandroid/repositories/house_repository.dart';
 import 'package:codingchallenge_redandroid/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'navigation/routes.dart';
 
@@ -8,13 +10,16 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      routes: {
-        Routes.splash.route: (_) => const SplashScreen(),
-        Routes.main.route: (_) => Container(),
-        Routes.details.route: (_) => Container(),
-      }
+    return RepositoryProvider(
+      create: (context) => HouseRepository(),
+      child: MaterialApp(
+          theme: ThemeData(primarySwatch: Colors.red),
+          routes: {
+            Routes.splash.route: (_) => const SplashScreen(),
+            Routes.main.route: (_) => Container(),
+            Routes.details.route: (_) => Container(),
+          }
+      ),
     );
   }
 }
