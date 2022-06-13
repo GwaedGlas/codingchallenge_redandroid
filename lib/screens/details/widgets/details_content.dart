61e4f6ee97957d0142eba67cd3bf3f254eeb0328
GwaedGlas <46369745+GwaedGlas@users.noreply.github.com>
2022-06-13 18:48:20 +0100

    Added Padding to details page and gave details a maximum width (corresponding to the left space)

--- lib/screens/details/widgets/details_content.dart
+++ lib/screens/details/widgets/details_content.dart
@@ -4,45 +4,70 @@ import '../../../service/houses/data/house.dart';
 
 class DetailsContent extends StatelessWidget {
   final House house;
+
   const DetailsContent({super.key, required this.house});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildTextInformation(context, "Service URL", house.url),
+          Divider(),
+          _buildBooleanInformation(context, "Died out", house.diedOut.isNotEmpty),
+          Divider(),
+          _buildTextInformation(context, "Region", house.region),
+          Divider(),
+          _buildTextInformation(context, "Words", house.words),
+          Divider(),
+          _buildTextInformation(context, "Coat of arms", house.coatOfArms),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextInformation(
+      BuildContext context, String title, String details) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        _buildTextInformation(context, "Service URL", house.url),
-        _buildBooleanInformation(context, "Died out", house.diedOut.isNotEmpty),
-        _buildTextInformation(context, "Region", house.region),
-        _buildTextInformation(context, "Words", house.words),
-        _buildTextInformation(context, "Coat of arms", house.coatOfArms),
+        Text(
+          "$title: ",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        Flexible(child: Text(details)),
       ],
     );
   }
 
-  Widget _buildTextInformation(BuildContext context, String title, String details) {
+  Widget _buildBooleanInformation(
+      BuildContext context, String title, bool check) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text("$title: ", style: Theme.of(context).textTheme.bodyLarge,),
-        Text(details),
-      ],
-    );
-  }
-
-  Widget _buildBooleanInformation(BuildContext context, String title, bool check) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text("$title: ", style: Theme.of(context).textTheme.bodyLarge,),
-        _getBooleanIcon(check)
+        Text(
+          "$title: ",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        Flexible(
+          child: _getBooleanIcon(check),
+        ),
       ],
     );
   }
 
   Icon _getBooleanIcon(bool check) {
-    if(check) {
-      return const Icon(Icons.check, color: Colors.green,);
+    if (check) {
+      return const Icon(
+        Icons.check,
+        color: Colors.green,
+      );
     }
-    return const Icon(Icons.close, color: Colors.red,);
+    return const Icon(
+      Icons.close,
+      color: Colors.red,
+    );
   }
 }
