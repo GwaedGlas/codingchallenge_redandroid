b7cf4edbb49fda7a88dee791fceaea58ca64fb1e
GwaedGlas <46369745+GwaedGlas@users.noreply.github.com>
2022-06-13 18:12:05 +0100

    Added content for splash screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,13 +1,22 @@
 import 'package:codingchallenge_redandroid/screens/splash/widgets/splash_content.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../repositories/house_repository.dart';
+import 'cubit/splash_cubit.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SplashContent(),
+    final houseRepository = context.read<HouseRepository>();
+    return Scaffold(
+      body: BlocProvider<SplashCubit>(
+        create: (_) => SplashCubit(houseRepository)..initialize(),
+        lazy: false,
+        child: const SplashContent(),
+      ),
     );
   }
 }
--- lib/screens/splash/widgets/splash_content.dart
+++ lib/screens/splash/widgets/splash_content.dart
@@ -1,10 +1,54 @@
+import 'package:codingchallenge_redandroid/screens/splash/cubit/splash_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../navigation/routes.dart';
 
 class SplashContent extends StatelessWidget {
   const SplashContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocListener<SplashCubit, SplashState>(
+      listener: (context, state) {
+        if (state is SplashLoaded) {
+          Navigator.of(context).pushReplacementNamed(Routes.main.route);
+        }
+      },
+      child: Container(
+        color: Theme.of(context).colorScheme.secondary,
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/images/iron_throne.png"),
+              const SizedBox(
+                height: 16,
+              ),
+              Text("The Houses of Thrones",
+                  style: Theme.of(context).textTheme.headline4),
+              BlocBuilder<SplashCubit, SplashState>(
+                builder: (context, state) {
+                  return _getStateWidget(state);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getStateWidget(SplashState state) {
+    if (state is SplashInitializing) {
+      return const Text("API Service initializing");
+    }
+    if (state is SplashLoading) {
+      return const Text("Loading House information");
+    }
+    if (state is SplashException) {
+      return Text("Error: ${state.exception}");
+    }
+
+    return const SizedBox();
   }
 }
